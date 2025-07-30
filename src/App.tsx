54cd956a19dcc6cd50655a381b59ca5eed654af9
Maslin <maslinfar@gmail.com>
2025-07-29 18:17:27 -0700

    Improved icons and improved css for the info-tiles

--- src/App.tsx
+++ src/App.tsx
@@ -20,11 +20,11 @@ function formatNumberToLocale(value:Number) {
 
 function createHomeInfoTiles(tileData:IUSTileData) {
   const titleMap: Record<keyof IUSTileData, string[]> = {
-    totalBackyardFlocksNationwide: ["Backyard Flocks Affected", "/backyard-flocks.png"],
-    totalBirdsAffectedNationwide: ["Birds Affected", "/birds-affected.png"],
-    totalCommercialFlocksNationwide: ["Commercial Flocks Affected", "/commercial-flocks.png"],
-    totalFlocksAffectedNationwide: ["Total Flocks Affected", "/flocks-affected.webp"],
-    totalStatesAffected: ["States Affected", "/us-states.png"],
+    totalBackyardFlocksNationwide: ["Backyard Flocks Affected", "/backyard-flocks2.png", "rgba(2, 163, 56, 1)"],
+    totalBirdsAffectedNationwide: ["Birds Affected", "/birds-affected.png", "#ef8700ff"],
+    totalCommercialFlocksNationwide: ["Commercial Flocks Affected", "/commercial-flocks.png", "rgba(131, 0, 239, 1)"],
+    totalFlocksAffectedNationwide: ["Total Flocks Affected", "/flocks-affected.webp", "rgba(255, 97, 131, 1)"],
+    totalStatesAffected: ["States Affected", "/us-states.png", "hsla(192, 98%, 37%, 1.00)"],
   };
 
   const infoTilesArr = Object.entries(tileData).map(([key, value], index) => {
@@ -33,7 +33,7 @@ function createHomeInfoTiles(tileData:IUSTileData) {
       console.error(`Unexpected key in tileData: ${key}`);
       return null;
     }
-    return <InfoTiles key={index} title={title[0]} amount={formatNumberToLocale(value)} icon={title[1]}/>
+    return <InfoTiles key={index} title={title[0]} amount={formatNumberToLocale(value)} icon={title[1]} bgColor={title[2]}/>
   }).filter(Boolean);
   
   return infoTilesArr
--- src/Components/InfoTiles/InfoTiles.tsx
+++ src/Components/InfoTiles/InfoTiles.tsx
@@ -1,9 +1,19 @@
-export default function InfoTiles({title, amount, icon}) {
+interface InfoTileProps {
+    title: string;
+    amount: string;
+    bgColor: string;
+    icon: string;
+}
+export default function InfoTiles({title, amount, bgColor, icon}: InfoTileProps) {
     return (
-        <div>
-            <p>{title}</p>
-            <h3>{amount}</h3>
-            <img src={icon}/>
+        <div style={{backgroundColor: bgColor}} className="tile-container" title={title}>
+            <div className="tile-inner">
+                <p>{title}</p>
+                <h3><span>{amount}</span></h3>
+            </div>
+            <div className="tile-icon">
+                <img src={icon} alt={`${title} icon`}/>
+            </div>
         </div>
-    )
+    );
 }
